0945af8d62c3e852e43a1387ef57d5a2fa619b1c
crollo94 <kelly.s826508@stu.sanjac.edu>
2025-04-02 21:02:21 +0000

    changed my loop and added a restart

--- dungeon_adventure.cpp
+++ dungeon_adventure.cpp
@@ -10,6 +10,9 @@ int main()
     char lookback_noflash;
     char lookback_flash;
     char wallspot;
+    char door;
+    bool gameover = true;
+    char restart;
 
 
     cout << "Welcome to the dungen" << endl;  // intro to my game
@@ -22,13 +25,23 @@ int main()
     cout << "Good luck" << endl;
 
     //it begins here
+    while (gameover)
+    {
+
+
+
     cout << "you walk down a dark path and notice a lightsource ahead." << endl;
     cout << " Do you want to go towards it or avoid it? (press w to go to the light and q to avoid)" << endl; // w = yes and q = no
     cin >> option; //this is the first user input and begins my if else statments
 
-    if (option == 'w')
+    switch (option)
     {
-        cout << "you have chosen to move towards the light." << endl;
+    
+    
+        case 'w':
+            {
+    
+            cout << "you have chosen to move towards the light." << endl;
 
         cout <<"you start to notice somthing under the light as you get closer." << endl;
         cout << "once closer you notice its a wood chest" << endl;
@@ -45,55 +58,92 @@ int main()
 
             if (lookback_flash == 'y')
             {
+            
                 cout << "you turn around and flash the ghostly figure with the flash light" << endl;
                 cout << "the light from the flashlight makes the ghost pause in place and disapre!" << endl;
                 cout << "deafting the ghost has made a spot on the wall shine do you want to check it out? y for yes and n for no" << endl;
                 cin >> wallspot;
-        
-        {
-            cout << "you have chosen to ignore the chest and move to the next area" <<endl;
-
-        }
 
                 if (wallspot == 'y')
                 {
                     cout << "you start to walk towards the shinning spot on the wall" << endl;
                     cout << " as you walk towards the wall it opens up and takes you outside" << endl;
                     cout << " You have escaped the dungen!! Good job!"<< endl;
+                    gameover= false;
+                }    
+                else
+                {
+                    cout << "you choose not to look at the wall and went a diffrent way" << endl;
+                    cout << "after walking around, you find a camp site to rest where a hero saves you! good job!" << endl;
+                    gameover = false;
                 }
             }
-        }else
-        {
-            cout << "" << endl;
+            else
+            {
 
+                    cout <<" you choose not to turn around and walk more into the darkness" << endl;
+                    cout << " The footsteps get louder and faster untill your knocked down and dragged away" <<endl;
+                    gameover = false;
+                }
+            }
+            else
+            {
+                cout << "you choose to not open the chest" << endl;
+                cout << " your chooice leds you to a wall with a door" << endl;
+                cout << " open the door? y for yes and n for no" << endl;
+                cin >> door;
+                
+                if (door == 'y')
+                {
+                    cout << "you opened the door and a bright light hits you upon opening your eyes you see gold!" << endl;
+                    cout << " you escape with gold!" << endl;
+                    gameover = false;
+                }
+            }
         }
-    }    
-    else if (option == 'q')
-    {   
-            cout << "you have decided to ignore the light and move on in the darkness" << endl;
-            cout << "out of no where you hear slow foot steps coming from behind you" << endl;
-            cout << "turn around? y for yes and n for no" << endl;
-            cin >> lookback_noflash;
-
+        break;
+        case 'q':
+        {
+            cout << "you have choose to ignore the light and move on into the darkness" << endl;
+            cout << " out of no where you hear slow heavy footsteps coming from behind you" << endl;
+            cout << " turn around? y for yes and n for no" << endl;
 
             if (lookback_noflash == 'y')
             {
-                cout << " you begin to turn around slowly feeling the hot air begin to chill and your skin begins to shiver" <<endl;
-            }  else
-               {
-                cout << " you have deiced to ignore the footsteps and move faster" << endl;
-               }          
-
+                cout << "you begin to turn around slowly, feeling the hot air turn cold" << endl;
+                cout << "you have nothing to prtoect your self with" << endl;
+                cout << "the ghost consume you in the darkness!" << endl;
+                gameover = false;
+            }
+            else
+            {
+                cout << "you have choose to ignore the foot steps and move faster" << endl;
+            }
+            gameover= false;
+        }
+        break;
+    default:
+        {
+        cout << "Invaild option" << endl;
+        }
+        break;
     }
-    else
-    {
-        cout << "invaild option please try again" <<endl;
-    }
+ }
+        if (gameover = true)
+        {
+            cout <<" do you want to play again? y for yes and n for no" << endl;
+            cin >> restart;
 
+            if (restart == 'y')
+            {
+                gameover = true;
+            }
+            else
+            {
+                cout << "thank you for trying my game!" << endl;
+            }
+        }
 
     return 0;
-}
-    
 
-    
-    
+}          
